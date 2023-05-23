373e23a4e030fce988fc23ad32bb93adf3b84876
Alejandro Rodriguez Hernandez <alejarod01@gmail.com>
2023-05-23 14:45:50 +0200

    Create method added, successfully saves the task to the DB and redirects back to the tasks list

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#index", as: :tasks
-  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/new", to: "tasks#new",       as: :new
+  post "tasks", to: "tasks#create",       as: :create
+  get "tasks", to: "tasks#index",         as: :tasks
+  get "tasks/:id", to: "tasks#show",      as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
 end
